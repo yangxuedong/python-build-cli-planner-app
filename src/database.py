c80286c1ee7e8000f5a979fdc71bfa62c6107f05
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-12 23:29:57 -0600

    Step #8

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import csv
 from src.reminder import PoliteReminder
+from src.deadlined_reminders import DateReminder
 
 
 def list_reminders():
@@ -14,9 +15,11 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text):
-    reminder = PoliteReminder(text)
+
+def add_reminder(text, date):
+    reminder = DateReminder(text, date)
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([reminder.text])
+        writer.writerow(reminder)
+
--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,5 +1,7 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
+from dateutil import parser
+from datetime import datetime
 
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
@@ -7,3 +9,24 @@ class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
     @abstractmethod
     def is_due(self):
         pass
+
+
+class DeadlinedReminder(ABC, Iterable):
+
+    @abstractmethod
+    def is_due(self):
+        pass
+
+
+class DateReminder(DeadlinedReminder):
+
+    def __init__(self, text, date):
+        self.date = parser(date, dayfirst=True)
+        self.text = text
+
+    def is_due(self):
+        return self.date <= datetime.now()
+
+    def __iter__(self):
+        formatted_date = self.date.isoformat()
+        return iter([text, formatted_date])
